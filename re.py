cc6c5b0d63869df673b1a5f55ca4b9b12001a471
jagueto98 <61714071+jagueto98@users.noreply.github.com>
2020-03-08 20:51:26 -0500

    Add files via upload

--- re.py
+++ re.py
@@ -8,5 +8,7 @@ script, patron, cadena = argv
 string = cadena.replace(",", " ")
 #patrones = [r'\b[a-z][0-9][0-9][a-z]\b',r'\b[0-9][0-9][0-9]\b']
 #busqueda_patron(patrones, string)
-print patron
+
+
+patron = r"\b" + patron + r"\b"
 print re.findall(r""+patron, string)
