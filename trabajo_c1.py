1bb5c1ccee513886f82dd8859fff7ab71000e03e
jagueto98 <61714071+jagueto98@users.noreply.github.com>
2020-03-27 12:35:41 -0500

    Se modificó el patrón de la expresión regular
    -Se modificó un parámetro en el programa que estaba generando una obtención infinita en el ciclo while.

---
+++ trabajo_c1.py
@@ -0,0 +1,24 @@
+import re
+class validarPass():
+    def __init__(self):
+        print("Validar contraseña...\n")
+    def validar(self):
+        self.passw = str(input("Ingrese la contraseña: "))
+        self.patron = r"^[A-Z][0-9][0-9][0-9][a-z][a-z][a-z](?:[!@#$%^&*(),.¿¡?:{}|<>]{3})$"
+        self.response = bool(re.search(self.patron, self.passw))
+        if (self.response):
+            print("La contraseña es correcta\n")
+            self.sw = str(input("Desea continuar?[s/n]"))
+            return self.sw
+        else:
+            print("Contraseña incorrecta.\n")
+            self.sw = str(input("Desea continuar?[s/n]"))
+            return self.sw
+app = validarPass()
+sw = app.validar()
+
+while(sw=="s"):
+    app = validarPass()
+    sw = app.validar()
+
+        
